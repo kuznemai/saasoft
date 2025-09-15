ef4bde291583a8dbc8779275ec2347626252cbad
Marina <marinakuznetsova420@gmail.com>
2025-09-15 20:25:23 +0300

    validation had been added

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useAccountsStore } from "@/store/accounts.js";
 import AccountItem from "@/components/AccountItem.vue";
+import { QuestionCircleOutlined } from "@ant-design/icons-vue";
 
 const store = useAccountsStore();
 
@@ -13,7 +14,16 @@ function getEmit(payload) {
   <div class="wrapper">
     <div class="wrapper_button">
       <p>Учетные записи</p>
-      <a-button type="primary" @click="store.addAccount()">+</a-button>
+      <a-button type="primary" @click="store.addAccount()" class="custom_addbtn"
+        >+</a-button
+      >
+    </div>
+    <div class="info-block">
+      <question-circle-outlined />
+      <span>
+        Для указания нескольких меток для одной пары логин/пароль используйте
+        разделитель ;
+      </span>
     </div>
     <div v-for="item in store.accounts" :key="item.id" class="account_item">
       <AccountItem :account="item" @update:model-value="getEmit"></AccountItem>
@@ -25,8 +35,10 @@ function getEmit(payload) {
 .wrapper {
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 50px;
 }
 .wrapper_button {
   display: flex;
@@ -36,5 +48,23 @@ function getEmit(payload) {
 }
 .account_item {
   margin: 10px;
+}
+.info-block {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 12px;
+  background-color: #cccccc;
+  font-size: 14px;
+  padding: 5px;
+}
+.info-block .anticon {
+  margin-right: 6px;
+  color: #1677ff;
+  font-size: 16px;
+}
+.custom_addbtn {
+  background-color: #ffffff;
+  color: #282828;
+  border: 1px solid #cdcdcd;
 }
 </style>
--- src/components/AccountItem.vue
+++ src/components/AccountItem.vue
@@ -1,42 +1,122 @@
 <script setup>
 import { useAccountsStore } from "@/store/accounts.js";
+import { reactive } from "vue";
+import { DeleteOutlined } from "@ant-design/icons-vue";
 const store = useAccountsStore();
 
 const props = defineProps({
   account: Object,
 });
+
+const errors = reactive({
+  login: false,
+  password: false,
+  type: false,
+});
+function validate() {
+  errors.login = false;
+  errors.password = false;
+  errors.type = false;
+  props.account.login = props.account.login.trim();
+  if (!props.account.login || props.account.login.trim() === "") {
+    errors.login = true;
+  }
+
+  if (!props.account.type) {
+    errors.type = true;
+  }
+
+  if (props.account.type === "Локальная") {
+    if (!props.account.password || props.account.password.trim() === "") {
+      errors.password = true;
+    } else {
+      props.account.password = null;
+    }
+  }
+}
+
+function trimOnBlur(field) {
+  if (props.account[field] && typeof props.account[field] === "string") {
+    props.account[field] = props.account[field].trim();
+  }
+  validate();
+}
 </script>
 
 <template>
-  <a-space class="mb-2" wrap>
+  <div class="account-row">
     <a-input
       v-model:value="props.account.labelsString"
       placeholder="Метки через ;"
       :maxlength="50"
+      @blur="trimOnBlur('labelsString')"
+      style="width: 150px"
     />
     <a-select
       v-model:value="props.account.type"
       style="width: 140px"
       placeholder="Тип записи"
+      :status="errors.type ? 'error' : ''"
     >
       <a-select-option value="Локальная">Локальная</a-select-option>
       <a-select-option value="LDAP">LDAP</a-select-option>
     </a-select>
-    <a-input
-      v-model:value="props.account.login"
-      placeholder="Логин"
-      :maxlength="100"
-    />
-    <a-input-password
-      v-if="props.account.type === 'Локальная'"
-      v-model:value="props.account.password"
-      placeholder="Пароль"
-      :maxlength="100"
-    />
-    <a-button danger @click="store.removeAccount(props.account.id)"
-      >Удалить</a-button
+
+    <!-- Flex контейнер для логина и пароля -->
+    <div class="login-password-wrapper">
+      <a-input
+        v-model:value="props.account.login"
+        placeholder="Логин"
+        :maxlength="100"
+        @blur="trimOnBlur('login')"
+        :status="errors.login ? 'error' : ''"
+      />
+      <a-input-password
+        v-if="props.account.type === 'Локальная'"
+        v-model:value="props.account.password"
+        placeholder="Пароль"
+        :maxlength="100"
+        @blur="trimOnBlur('password')"
+        :status="errors.password ? 'error' : ''"
+      />
+    </div>
+
+    <a-button
+      danger
+      type="text"
+      @click="store.removeAccount(props.account.id)"
+      class="custom-button"
     >
-  </a-space>
+      <delete-outlined />
+    </a-button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.custom-button {
+  color: gray;
+  font-size: 20px;
+  padding: 0 10px;
+}
+
+.custom-button:hover {
+  color: darkgray;
+}
+.account-row {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  width: 100%;
+}
+
+.login-password-wrapper {
+  display: flex;
+  gap: 10px;
+  flex: 1;
+}
+
+.login-password-wrapper a-input,
+.login-password-wrapper a-input-password {
+  flex: 1;
+}
+</style>
